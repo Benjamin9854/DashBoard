f54c2b43962fa19d40cafab6cae4f22845dc0d50
Pablo <pablonikolazz@gmail.com>
2024-05-29 12:35:26 -0300

    first commit

--- tables.php
+++ tables.php
@@ -239,16 +239,6 @@ $total_pages = ceil($total_records / $records_per_page);
             </div>
             <!-- End of Main Content -->
 
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
-
         </div>
         <!-- End of Content Wrapper -->
 
